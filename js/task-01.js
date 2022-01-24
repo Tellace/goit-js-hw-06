7864dc4853781378015ffdc68b67ce063c58b1ec
Ivan Timko <ivan.timko1337@gmail.com>
2022-01-24 08:50:37 +0300

    first_save

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const allCategories = document.querySelectorAll('ul#categories li.item');
+const numberOfCategories = allCategories.length;
+console.log('Number of categories: ${numberOfCategories}');
+
+for (let i = 0; i < numberOfCategories; i += 1) {
+const headText = allCategories[i].firstElementChild.textContent; 
+console.log('Category: ${headText}');
+
+const elementsEl = allCategories[i].lastElementChild;
+const numberOfElements = elementsEl.querySelectorAll('li');
+console.log('Elements: ${numberOfElements.length}');
+}
