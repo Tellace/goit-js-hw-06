2995a7215daeff3b3ef382d4082a6169d633698d
Ivan Timko <ivan.timko1337@gmail.com>
2022-02-08 08:27:44 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
-const allCategories = document.querySelectorAll('ul#categories li.item');
+const allCategories = document.querySelectorAll(`ul#categories li.item`);
 const numberOfCategories = allCategories.length;
-console.log('Number of categories: ${numberOfCategories}');
+console.log(`Number of categories: ${numberOfCategories}`);
 
 for (let i = 0; i < numberOfCategories; i += 1) {
 const headText = allCategories[i].firstElementChild.textContent; 
-console.log('Category: ${headText}');
+console.log(`Category: ${headText}`);
 
 const elementsEl = allCategories[i].lastElementChild;
-const numberOfElements = elementsEl.querySelectorAll('li');
-console.log('Elements: ${numberOfElements.length}');
+const numberOfElements = elementsEl.querySelectorAll(`li`);
+console.log(`Elements: ${numberOfElements.length}`);
 }
